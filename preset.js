1461b0a427e674458c09d5c8394472209a571af3
nicklassundin <ada08njo@student.lu.se>
2021-09-21 11:16:14 +0200

    merge

--- preset.js
+++ preset.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var merge = require('merge');
 var normalizedPath = require("path").join(__dirname, "preset");
 exports.preset = new Promise((resolve, reject) => {
-	console.log(normalizedPath)
 	fs.readdir(normalizedPath, (err, files) => {
 		Promise.all(files.filter((name) => { return name.includes(".json") })
 			.map(function(file) {
