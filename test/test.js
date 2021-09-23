ab0760ed17665df8ad41ec4ee439d736f8238b68
nicklassundin <ada08njo@student.lu.se>
2021-09-23 10:46:17 +0200

    fix(testing) fix testing

--- test/test.js
+++ test/test.js
@@ -1,13 +1,29 @@
 const app = require('../app.js');
 
 describe('test build', (done) => {
-  it('load and write', (done) => {
-    app.genStaticFiles(__dirname).then((res) => {
-      // expect(0).to.eql(10)
-      console.log(res);
-      done();
-    }).catch((error) => {
-      done(error);
-    });
-  });
+	it('load and write', (done) => {
+		const fs = require('fs');
+
+		// directory path
+		// create new directory
+		fs.mkdir('./test/static', (err) => {
+			if (err) {
+				throw err;
+			}
+			console.log("Directory is created.");
+		});
+		fs.mkdir('./test/static/charts', (err) => {
+			if (err) {
+				throw err;
+			}
+			console.log("Directory is created.");
+		});
+		app.genStaticFiles(__dirname).then((res) => {
+			// expect(0).to.eql(10)
+			console.log(res);
+			done();
+		}).catch((error) => {
+			done(error);
+		});
+	});
 });
