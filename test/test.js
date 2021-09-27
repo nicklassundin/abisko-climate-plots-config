c52602ea75e8d9c7581d53f58d2c6f2ebb3a756b
nicklassundin <ada08njo@student.lu.se>
2021-09-27 12:15:13 +0200

    hotfix tests

--- test/test.js
+++ test/test.js
@@ -1,33 +1,38 @@
 const app = require('../app.js');
 const fs = require('fs');
 
-describe('test build', (done) => {
-	before(function() {
+describe('test build', () => {
+	before(function(done) {
 		if(!fs.existsSync('./test/static')) fs.mkdir('./test/static', (err) => {
 			if (err) {
 				throw err;
 			}
+			done()
 			console.log("Directory is created.");
 		});
 		if(!fs.existsSync('./test/static/charts')) fs.mkdir('./test/static/charts', (err) => {
 			if (err) {
 				throw err;
+			}else{
+				done()
 			}
 			console.log("Directory is created.");
 		});
 	})
-	it('load and write', (done) => {
+	describe('testing', function() {
 
-		// directory path
-		// create new directory
-		app.genStaticFiles(__dirname).then((res) => {
-			// expect(0).to.eql(10)
-			console.log(res);
-			done();
-		}).catch((error) => {
-			done(error);
+		it('load and write', (done) => {
+			// directory path
+			// create new directory
+			app.genStaticFiles(__dirname).then((res) => {
+				// expect(0).to.eql(10)
+				console.log(res);
+				done();
+			}).catch((error) => {
+				done(error);
+			});
 		});
-	});
+	})
 	after(function() {
 		var dir = './test/static'
 		fs.rmdir(dir, { recursive: true }, (err) => {
